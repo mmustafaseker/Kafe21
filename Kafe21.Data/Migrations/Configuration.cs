371a9b9aa8a9e5c6caf2cad85dbf37b8f842d6e4
mustafa şeker <mustiseker.06@gmail.com>
2021-02-23 18:12:00 +0300

    Kafe21.Data EF6 Entegrasyonu

---
+++ Kafe21.Data/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Kafe21.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Kafe21.Data.KafeVeri>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Kafe21.Data.KafeVeri context)
+        {
+            if (!context.Urunler.Any())
+            {
+                context.Urunler.Add(new Urun() { UrunAd = "fanta", BirimFiyat = 5.99m });
+                context.Urunler.Add(new Urun() { UrunAd = "tonik", BirimFiyat = 8.99m });
+            }
+        }
+    }
+}
--- Kafe21.Data/Urun.cs
+++ Kafe21.Data/Urun.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Kafe21.Data
 {
+    [Table("Urunler")]
     public class Urun
     {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
         public string UrunAd { get; set; }
         public decimal BirimFiyat { get; set; }
 
@@ -15,5 +20,7 @@ namespace Kafe21.Data
         {
             return string.Format("{0} ({1:c2})", UrunAd, BirimFiyat);
         }
+
+        public virtual ICollection<SiparisDetay> SiparisDetaylar { get; set; }
     }
 }
